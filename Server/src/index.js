8dd144e09d45a802798f8c6a4dc96853aba2c948
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-12-15 20:22:18 +0530

    changes for deployment

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 
 dotenv.config('./.env');
 // server starts
-
+app.get('/', (req, res) => {
+    res.send('Hello, World! This is a basic Express server.');
+});
 connectDatabase().then(() => {
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
