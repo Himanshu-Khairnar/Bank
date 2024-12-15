adb9b0dfa1a0740ae6e85f5d0ae6bab7f1d7149e
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-12-15 21:15:26 +0530

    deployment issue

--- Server/src/controllers/Account.Controller.js
+++ Server/src/controllers/Account.Controller.js
@@ -1,9 +1,9 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/AsyncHandler.js";
 import { pool } from "../Database.js";
 
-export const AllAccounts = asyncHandler(async (req, res, next) => {
+export const AllAccounts = asyncHandler(async (req, res,) => {
     try {
         const query = `
             SELECT 
